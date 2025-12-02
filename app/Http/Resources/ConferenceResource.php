b6f32345d1af608963705092f85b7de8bdf07ce5
Lerior <leonardogarcia3500@gmail.com>
2025-12-02 15:40:00 -0600

    feat: add  topic and conference resources

---
+++ app/Http/Resources/ConferenceResource.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ConferenceResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'title' => $this->title,
+            'description' => $this->description,
+            'owner' => [                
+                'id' => $this->id,
+                'name' => $this->user->name,
+            ],
+            'topics' => TopicResource::collection($this->topics)
+        ];
+    }
+}
---
+++ app/Http/Resources/TopicResource.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class TopicResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+       return [
+        'id' => $this->id,
+        'title' => $this->title,
+        'description' => $this->description,
+        'author' => [
+            'id' => $this->user?->id,
+            'name' => $this->user?->name ?? $this->speaker_name,
+        ],
+      ];
+    }
+}
